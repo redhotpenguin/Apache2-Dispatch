960eb9b3a211afc17844f6332cd766bb02237162
phred_moyer <phred_moyer>
2006-04-23 03:10:10 +0000

    Ok geoff you can stop cringing from my previous checkins, I understand how the xs voodoo works now.  the makefile.pl wasn't in Apache::Dispatch namespace, it was in main namespace since the first line was shebang perl...

--- lib/Apache/Dispatch.pm
+++ lib/Apache/Dispatch.pm
@@ -11,110 +11,7 @@ package Apache::Dispatch;
 use strict;
 use warnings;
 
-my $VERSION = '0.10';
-
-my @directives = (
-
-    #------------------------------------------------------------------
-    # DispatchPrefix defines the base class for a given <Location>
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchPrefix',
-     errmsg       => 'a class to be used as the base class',
-     args_how     => 'TAKE1',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchExtras defines the extra dispatch methods to enable
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchExtras',
-     errmsg       => 'choose any of: Pre, Post, or Error',
-     args_how     => 'ITERATE',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchStat enables module testing and subsequent reloading
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchStat',
-     errmsg       => 'choose one of On, Off, or ISA',
-     args_how     => 'TAKE1',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchAUTOLOAD defines AutoLoader behavior
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchAUTOLOAD',
-     errmsg       => 'choose one of On or Off',
-     args_how     => 'FLAG',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchDebug defines debugging verbosity
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchDebug',
-     errmsg       => 'numeric verbosity level',
-     args_how     => 'TAKE1',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchISA is a list of modules your module should inherit from
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchISA',
-     errmsg       => 'a list of parent modules',
-     args_how     => 'ITERATE',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchLocation allows you to redefine the <Location>
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchLocation',
-     errmsg       => 'a location to replace the current <Location>',
-     args_how     => 'TAKE1',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchRequire require()s the class
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchRequire',
-     errmsg       => 'choose one of On or Off',
-     args_how     => 'FLAG',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchFilter makes the dispatched handler Apache::Filter aware
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchFilter',
-     errmsg       => 'choose one of On or Off',
-     args_how     => 'FLAG',
-     req_override => 'OR_ALL',
-    },
-
-    #------------------------------------------------------------------
-    # DispatchUppercase converts the first char of a class to uppercase
-    #------------------------------------------------------------------
-    {
-     name         => 'DispatchUpperCase',
-     errmsg       => 'choose one of On or Off',
-     args_how     => 'FLAG',
-     req_override => 'OR_ALL',
-    },
-);
+our $VERSION = '0.10';
 
 use mod_perl 1.2401;
 use Apache::Constants qw(OK DECLINED SERVER_ERROR);
@@ -129,7 +26,7 @@ if ($Apache::Dispatch::PUREPERL == 0) {
     require Apache::ModuleConfig;
     require DynaLoader;
     @Apache::Dispatch::ISA = qw(DynaLoader);
-    __PACKAGE__->bootstrap($VERSION);
+	__PACKAGE__->bootstrap($VERSION);
 }
 
 sub directives {
